1c57ba5af7e39031cd7d1e85b76c841f2a12cce4
thanusha25 <thanusharamesh.25@gmail.com>
2019-11-14 16:28:14 +0100

    Updates

---
+++ precache-manifest.a1fb64fea17766a77322fc8aec812600.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1225fbe35bc398caae077c71e66b698f",
+    "url": "/React-Basics/index.html"
+  },
+  {
+    "revision": "44bde8ba0ad3e22f44e0",
+    "url": "/React-Basics/static/css/main.4640ecc5.chunk.css"
+  },
+  {
+    "revision": "c4de08c8e1140d25c0ae",
+    "url": "/React-Basics/static/js/2.5905bb17.chunk.js"
+  },
+  {
+    "revision": "44bde8ba0ad3e22f44e0",
+    "url": "/React-Basics/static/js/main.708310d6.chunk.js"
+  },
+  {
+    "revision": "64e49a4f5247438abca6",
+    "url": "/React-Basics/static/js/runtime-main.f643b128.js"
+  }
+]);
